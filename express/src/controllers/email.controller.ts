e1f4432a67cd75d977bd3b82757343f58e3c4217
Richard Chou <richard@ezyraise.com>
2022-09-27 11:17:49 +1000

    [test] add some code

--- express/src/controllers/email.controller.ts
+++ express/src/controllers/email.controller.ts
@@ -5,6 +5,17 @@ export async function emailHandler(
   req: Request,
   res: Response,
 ) {
-  
+  const client = new SNSClient({
+    region: "us-west-2",
+  });
+
+  const { emailEvents } = JSON.parse(process.env.COPILOT_SNS_TOPIC_ARNS as string);
+  console.log(emailEvents);
+
+  const out = await client.send(new PublishCommand({
+    Message: "sending message",
+    TopicArn: emailEvents,
+  }));
+
   return res.status(200).send();
 }
